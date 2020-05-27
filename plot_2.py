ddd259da399169ad652694658e878ad57d13b658
pmaillefert <61688906+pmaillefert@users.noreply.github.com>
2020-05-27 11:15:25 +0200

    Update plot_2.py

--- plot_2.py
+++ plot_2.py
@@ -6,7 +6,7 @@ import io
 from functions import *
 
 
-def generate_svg_plot(dictionary, min, max, liste_cord, width, choice):
+def generate_svg_plot2(dictionary, min, max, liste_cord, width, choice):
 
     # img
     imgdata = io.BytesIO()
