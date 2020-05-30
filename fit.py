eaae4f9774bbf288a2beac48fa27f51eb4d35eee
pmaillefert <61688906+pmaillefert@users.noreply.github.com>
2020-05-30 22:30:36 +0200

    Update fit.py

--- fit.py
+++ fit.py
@@ -62,8 +62,7 @@ def regressions(liste_cord, dictionnaire={}):
             # calcul et affichage du mean squared error et du r2
             # print "Mean Squared Error exp : ", np.mean((y-funcexp(x,
             # *popt1))**2)
-            ss_res = np.dot(((y - funcexp(x, a1, b1, c1)).T),
-                            (y - funcexp(x, a1, b1, c1)))
+            ss_res = funcexp(x, a1, b1, c1)))
             ymean = np.mean(y)
             ss_tot = np.dot((y - ymean), (y - ymean))
             # ajout du r2 dans le dictionnaire pour la regression exponentielle
