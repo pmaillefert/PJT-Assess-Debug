92bf35bb7814306b8764550b3f8842470551e12f
pmaillefert <61688906+pmaillefert@users.noreply.github.com>
2020-05-30 21:53:38 +0200

    Update fit.py

--- fit.py
+++ fit.py
@@ -38,10 +38,40 @@ def regressions(liste_cord, dictionnaire={}):
         max = float(x[-2])
 
     # creation of the fitted curves
-
+dictionnaire = { 'exp' : {} ,'lin' : {},'log' : {},'pow' : {},'quad' : {},'expo-power' : {}}
     try:
-        
-            dictionnaire['exp']['r2'] = 1 
+        # exponential function
+        funcexpParam = lambda x, b: funcexp2(x, b, min, max)
+        # fonction regression utilisant la funcexp du fichier functions.py
+        popt1, pcov1 = curve_fit(funcexpParam, x, y, [0.1])
+        # popt1 = matrice ligne contenant les coefficients de la regression exponentielle optimisee apres calcul / popcov1 = matrice de covariances pour cette regression exp
+        # ajout des coeeficients a, b et c dans le dictionnaire pour la regression
+        # exponentielle
+        b1 = popt1[0]
+        a1 = (1. / (np.exp(-b1 * max) - np.exp(-b1 * min)))
+        c1 = (1. / (1 - np.exp(b1 * (min - max))))
+        # test de la fonction d'utilite qui doit etre comprise entre 0 et 1
+        test = True
+        for i in x_test:
+            if funcexp(i, a1, b1, c1) < -0.02 or funcexp(i, a1, b1, c1) > 1.02:
+                test = False
+        if np.isnan(a1) or np.isnan(b1) or np.isnan(c1):
+            test = False
+			
+        if test:
+            dictionnaire['exp'] = {}
+            dictionnaire['exp']['a'] = a1
+            dictionnaire['exp']['b'] = b1
+            dictionnaire['exp']['c'] = c1
+            # calcul et affichage du mean squared error et du r2
+            # print "Mean Squared Error exp : ", np.mean((y-funcexp(x,
+            # *popt1))**2)
+            ss_res = np.dot((y - funcexp(x, a1, b1, c1)),
+                            (y - funcexp(x, a1, b1, c1)))
+            ymean = np.mean(y)
+            ss_tot = np.dot((y - ymean), (y - ymean))
+            # ajout du r2 dans le dictionnaire pour la regression exponentielle
+            dictionnaire['exp']['r2'] = 1 - ss_res / ss_tot
     except:
         pass
 
